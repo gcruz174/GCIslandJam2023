4146244ccfd46f2ee62a42772c99dd6504aa2efe
Ginés Cruz <ginescruz174@gmail.com>
2023-12-16 11:39:12 +0000

    Destructible terrain

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,13 +1,57 @@
+using System;
+using DTerrain;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public LayerMask planetLayer;
+    public int circleSize = 16;
+    public int outlineSize = 4;
+    
+    protected Shape destroyCircle;
+    protected Shape outlineCircle;
+    
+    private BasicPaintableLayer primaryLayer;
+    private BasicPaintableLayer secondaryLayer;
+
+    private void Awake()
+    {
+        primaryLayer = GameObject.Find("CollidableLayer").GetComponent<BasicPaintableLayer>();
+        secondaryLayer = GameObject.Find("OutlineLayer").GetComponent<BasicPaintableLayer>();
+        
+        destroyCircle = Shape.GenerateShapeCircle(circleSize);
+        outlineCircle = Shape.GenerateShapeCircle(circleSize + outlineSize);
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Enemy"))
-        {
-            Destroy(other.gameObject);
-            Destroy(gameObject);
-        }
+        if (planetLayer != (planetLayer | (1 << other.gameObject.layer))) return;
+        
+        Vector3 p = transform.position - primaryLayer.transform.position;
+
+        primaryLayer?.Paint(new PaintingParameters() 
+        { 
+            Color = Color.clear, 
+            Position = new Vector2Int((int)(p.x * primaryLayer.PPU) - circleSize, (int)(p.y * primaryLayer.PPU) - circleSize), 
+            Shape = destroyCircle, 
+            PaintingMode=PaintingMode.REPLACE_COLOR,
+            DestructionMode = DestructionMode.DESTROY
+        });
+
+        secondaryLayer?.Paint(new PaintingParameters() 
+        { 
+            Color = new Color(0.0f,0.0f,0.0f,0.75f), 
+            Position = new Vector2Int((int)(p.x * secondaryLayer.PPU) - circleSize-outlineSize, (int)(p.y * secondaryLayer.PPU) - circleSize-outlineSize), 
+            Shape = outlineCircle, 
+            PaintingMode=PaintingMode.REPLACE_COLOR,
+            DestructionMode = DestructionMode.NONE
+        });
+            
+        Destroy(gameObject);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/DestroyPercentage.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class DestroyPercentage : MonoBehaviour
+{
+    public Transform collidableLayer;
+    public TMP_Text percentageText;
+    
+    private float _initialArea = -1f;
+    
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(1f);
+        _initialArea = CalculateBoxCollidersArea(collidableLayer.GetComponentsInChildren<BoxCollider2D>()) * 0.9f;
+    }
+    
+    private void Update()
+    {
+        if (_initialArea < 0f) return;
+        var currentArea = CalculateBoxCollidersArea(collidableLayer.GetComponentsInChildren<BoxCollider2D>());
+        var percentage = 100f - (currentArea / _initialArea) * 100;
+        percentageText.text = $"{percentage:0}%";
+    }
+    
+    private float CalculateBoxCollidersArea(IEnumerable<BoxCollider2D> boxColliders)
+    {
+        return boxColliders.Sum(boxCollider => boxCollider.size.x * boxCollider.size.y);
+    }
+}
