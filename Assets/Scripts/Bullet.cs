ea372d24424037be3e9c66d782a5f633d0c70888
Ginés Cruz <ginescruz174@gmail.com>
2023-12-16 12:52:42 +0000

    Añadidas capas

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -10,15 +10,9 @@ public class Bullet : MonoBehaviour
     
     protected Shape destroyCircle;
     protected Shape outlineCircle;
-    
-    private BasicPaintableLayer primaryLayer;
-    private BasicPaintableLayer secondaryLayer;
 
     private void Awake()
     {
-        primaryLayer = GameObject.Find("CollidableLayer").GetComponent<BasicPaintableLayer>();
-        secondaryLayer = GameObject.Find("OutlineLayer").GetComponent<BasicPaintableLayer>();
-        
         destroyCircle = Shape.GenerateShapeCircle(circleSize);
         outlineCircle = Shape.GenerateShapeCircle(circleSize + outlineSize);
     }
@@ -27,12 +21,14 @@ public class Bullet : MonoBehaviour
     {
         if (planetLayer != (planetLayer | (1 << other.gameObject.layer))) return;
         
-        Vector3 p = transform.position - primaryLayer.transform.position;
+        var primaryLayer = other.gameObject.GetComponentInParent<BasicPaintableLayer>();
+        var secondaryLayer = other.transform.parent.GetChild(0).GetComponent<BasicPaintableLayer>();
+        var hitPoint = transform.position - primaryLayer.transform.position;
 
         primaryLayer?.Paint(new PaintingParameters() 
         { 
             Color = Color.clear, 
-            Position = new Vector2Int((int)(p.x * primaryLayer.PPU) - circleSize, (int)(p.y * primaryLayer.PPU) - circleSize), 
+            Position = new Vector2Int((int)(hitPoint.x * primaryLayer.PPU) - circleSize, (int)(hitPoint.y * primaryLayer.PPU) - circleSize), 
             Shape = destroyCircle, 
             PaintingMode=PaintingMode.REPLACE_COLOR,
             DestructionMode = DestructionMode.DESTROY
@@ -41,7 +37,7 @@ public class Bullet : MonoBehaviour
         secondaryLayer?.Paint(new PaintingParameters() 
         { 
             Color = new Color(0.0f,0.0f,0.0f,0.75f), 
-            Position = new Vector2Int((int)(p.x * secondaryLayer.PPU) - circleSize-outlineSize, (int)(p.y * secondaryLayer.PPU) - circleSize-outlineSize), 
+            Position = new Vector2Int((int)(hitPoint.x * secondaryLayer.PPU) - circleSize-outlineSize, (int)(hitPoint.y * secondaryLayer.PPU) - circleSize-outlineSize), 
             Shape = outlineCircle, 
             PaintingMode=PaintingMode.REPLACE_COLOR,
             DestructionMode = DestructionMode.NONE
--- Assets/Scripts/DestroyPercentage.cs
+++ Assets/Scripts/DestroyPercentage.cs
@@ -14,14 +14,14 @@ public class DestroyPercentage : MonoBehaviour
     private IEnumerator Start()
     {
         yield return new WaitForSeconds(1f);
-        _initialArea = CalculateBoxCollidersArea(collidableLayer.GetComponentsInChildren<BoxCollider2D>()) * 0.9f;
+        _initialArea = CalculateBoxCollidersArea(collidableLayer.GetComponentsInChildren<BoxCollider2D>()) * 1.1f;
     }
     
     private void Update()
     {
         if (_initialArea < 0f) return;
         var currentArea = CalculateBoxCollidersArea(collidableLayer.GetComponentsInChildren<BoxCollider2D>());
-        var percentage = 100f - (currentArea / _initialArea) * 100;
+        var percentage = 100f - currentArea / _initialArea * 100;
         percentageText.text = $"{percentage:0}%";
     }
     
